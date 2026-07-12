7de1f2d84a29c62500f36093adb108143e26e718
prajwal53-tech <prajwalkubasad53@gmail.com>
2026-07-12 18:49:15 +0530

    login page

--- Backend/USMS/myapp/models.py
+++ Backend/USMS/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    enrollment_number = models.CharField(max_length=20, unique=True)
+    date_of_birth = models.DateField()
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
